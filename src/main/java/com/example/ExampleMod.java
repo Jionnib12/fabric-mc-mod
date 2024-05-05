759c5f3d0e043f62ef1cce3828f3b71fd46ad063
jionnib12 <149334824+Jionnib12@users.noreply.github.com>
2024-05-04 20:04:50 -0400

    GO TO THE LINK I AM GOING TO TELL COACH MIKE
    go to this link https://github.com/Jionnib12/Fabric-ProjectExecute-1.20.2 i can not make a new file with a pull request so this is the best i can do so just swap the stuff out i will be walking my dog and then play helldivers 2 50% of the night

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,7 +1,7 @@
 // Deenchant armor
 // make it so that the upgrade function and enchant armor works together with the correct order
 // order goes with armor enchant until end and upgrade to new armor
-
+// look at the comment not this
 
 package com.example;
 
